58d3ca1cd2e077041727bec961cdb37f10f99e7d
Giao Nguyen <giao.nguyenn@yahoo.com>
2017-12-13 16:41:10 -0600

    fixed jQuery API call with debounce

--- bookworm/src/components/SearchBooks/SearchInput/SearchInput.js
+++ bookworm/src/components/SearchBooks/SearchInput/SearchInput.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import { DebounceInput } from "react-debounce-input";
 import "./SearchInput.css";
 
 export default class SearchInput extends Component {
@@ -11,8 +11,8 @@ export default class SearchInput extends Component {
 					<h1>Search Books by Title or Author</h1>
 				</div>
 				<div className="input-field">
-					<input type="text" placeholder="Enter Keyword or Title" onChange={ (e) => { this.props.findBooks(e.target.value) } }></input>
-					<input type="text" placeholder="Enter Author's Name" onChange={ (e) => { this.props.findBooks(e.target.value) } }></input>
+					<DebounceInput minLength={ 2 } debounceTimeout={ 300 } type="text" placeholder="Enter Keyword or Title" onChange={ (e) => { this.props.findBooks(e.target.value) } } />
+					<DebounceInput minLength={ 2 } debounceTimeout={ 300 } type="text" placeholder="Enter Author's Name" onChange={ (e) => { this.props.findBooks(e.target.value) } } />
 				</div>
 			</div>
 		)
